cad9d7602322d0facf7c6582344ad7e117481fd6
CampusStudent <70265940+CampusStudent@users.noreply.github.com>
2024-08-05 14:53:15 +0200

    Update AppTest.java

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.*;
 
 import java.io.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*; // Test
 
 @Timeout(2)
 class AppTest {
